c5cced2a92da54d89d3775aba861b52473d59ea5
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2024-03-15 08:19:36 -0300

    Add i18n and devise_invitable

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module MuseumApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = [:en, :es]
+    config.i18n.default_locale = :es
   end
 end
