18b8adcc66aae1a8bbc7397bcf2a1045c24554b4
alexandra-lezcano <alexandra.lezcano@fiuni.edu.py>
2024-07-13 15:08:37 -0400

    Allow editing material

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,5 +1,75 @@
 class MaterialsController < ApplicationController
+  prepend_before_action :set_material, only: %i[ show edit update destroy ]
+
+  # GET /materials or /materials.json
   def index
     @materials = Material.all
+  end
+
+  # GET /materials/1 or /materials/1.json
+  def show
+  end
+
+  # GET /materials/new
+  def new
+    @material = Material.new
+  end
+
+  # GET /materials/1/edit
+  def edit
+  end
+
+  # POST /materials or /materials.json
+  def create
+    @material = Material.new(material_params)
+
+    respond_to do |format|
+      if @material.save
+        format.html { redirect_to materials_path, notice: "Material was successfully created." }
+        format.json { render :show, status: :created, location: @material }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @material.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /materials/1 or /materials/1.json
+  def update
+    respond_to do |format|
+      if @material.update(material_params)
+        format.html { redirect_to material_url(@material), notice: "Material was successfully updated." }
+        format.json { render :show, status: :ok, location: @material }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @material.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /materials/1 or /materials/1.json
+  def destroy
+    @material.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to materials_url, notice: "Material was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_material
+    @material = Material.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def material_params
+    params.require(:material).permit(:name)
+  end
+
+  def materialize_user!
+    not_authorized unless current_user.admin?
   end
 end
